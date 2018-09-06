739d1ab2a6be22695de8a4ee3c092754035f6f14
Anndo Ko <handcuffanndo@gmail.com>
2018-09-06 18:18:23 -0400

    fix about

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -6,9 +6,12 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'about.html'
 })
 export class AboutPage {
+  author: string;
+  now: Date;
 
   constructor(public navCtrl: NavController) {
-
+    this.author = 'Anndo Ko'; // replace with your name
+    this.now = new Date();
   }
 
 }
